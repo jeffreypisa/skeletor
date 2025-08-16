0f3ea9398df1ab1a2f7064ad141c427fa8cf2db6
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2025-08-16 12:29:45 +0200

    Simplify search page with post type filter

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Enhanced search results page with filter components and load more.
+ * Search results page with post type selector.
  *
  * @package WordPress
  * @subpackage Timber
@@ -9,7 +9,6 @@
 $templates = ['search.twig', 'archive.twig', 'index.twig'];
 
 $context = Timber::context();
-
 
 $posts_per_page = 12;
 $context['posts_per_page'] = $posts_per_page;
@@ -30,60 +29,34 @@ if (!in_array($post_type, $allowed_post_types, true)) {
 }
 $context['post_type'] = $post_type;
 
-$filters = [];
-
-if ($post_type === 'product') {
-    $filters['price'] = [
-        'name'   => '_price',
-        'label'  => 'Prijs',
-        'type'   => 'range',
-        'source' => 'meta',
-    ];
-    $filters['stock_status'] = [
-        'name'    => '_stock_status',
-        'label'   => 'Beschikbaarheid',
-        'type'    => 'select',
-        'source'  => 'meta',
-        'options' => [
-            'Op voorraad' => 'instock',
-            'Uitverkocht' => 'outofstock',
-        ],
-    ];
-} else {
-    $filters['provincies'] = [
-        'name'   => 'provincies',
-        'label'  => 'Provincies',
-        'type'   => 'checkbox',
-        'source' => 'taxonomy',
-        'hide_empty_options' => true,
-    ];
-    $filters['rating'] = [
-        'name'   => 'rating',
-        'label'  => 'Rating',
-        'type'   => 'range',
-        'source' => 'acf',
-    ];
-    $filters['published'] = [
-        'name'   => 'post_date',
-        'label'  => 'Periode',
-        'type'   => 'date_range',
-        'source' => 'post_date',
-    ];
+$post_type_options = [];
+foreach ($allowed_post_types as $type) {
+    $obj   = get_post_type_object($type);
+    $label = $obj ? $obj->labels->name : ucfirst($type);
+    $post_type_options[$label] = $type;
 }
 
-$context['filters'] = $filters;
+$context['filters'] = [
+    'post_type' => [
+        'name'   => 'post_type',
+        'label'  => 'Type',
+        'type'   => 'select',
+        'source' => 'acf',
+        'options'=> $post_type_options,
+        'value'  => $post_type,
+    ],
+];
+$context['ajax_filters'] = $context['filters'];
+set_transient('components_ajax_filters_' . $post_type, $context['filters'], DAY_IN_SECONDS);
+
+$context['search_query'] = get_search_query();
 
 $query_args = [
     'post_type'      => $post_type,
     'posts_per_page' => $posts_per_page,
     'paged'          => get_query_var('paged') ?: 1,
-    's'              => get_search_query(),
+    's'              => $context['search_query'],
 ];
-
-$query_args = array_merge(
-    $query_args,
-    Components_Filter::build_query_from_filters($filters)
-);
 
 $query = new WP_Query($query_args);
 
@@ -93,24 +66,4 @@ $context['current_page']  = get_query_var('paged') ?: 1;
 $context['max_num_pages'] = $query->max_num_pages;
 $context['title']         = 'Zoekresultaten voor ' . get_search_query();
 
-$context['ajax_filters'] = $filters;
-set_transient('components_ajax_filters_' . $post_type, $filters, DAY_IN_SECONDS);
-
-$post_type_options = [];
-foreach ($allowed_post_types as $type) {
-    $obj   = get_post_type_object($type);
-    $label = $obj ? $obj->labels->name : ucfirst($type);
-    $post_type_options[$label] = $type;
-}
-
-$context['filters']['post_type'] = [
-    'name'   => 'post_type',
-    'label'  => 'Type',
-    'type'   => 'select',
-    'source' => 'acf',
-    'options'=> $post_type_options,
-    'value'  => $post_type,
-];
-
 Timber::render($templates, $context);
-
