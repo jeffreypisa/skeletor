d9559d75b418aa10aa70412c57a69f38e9495ed3
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2025-08-17 18:35:46 +0200

    feat: create search result tease

--- components/library/filter/FilterAjax.php
+++ components/library/filter/FilterAjax.php
@@ -39,7 +39,24 @@ class Components_FilterAjax {
 			wp_die();
 		}
 	
-               $post_type      = sanitize_text_field($filters['post_type'] ?? 'post');
+               $post_type_param = sanitize_text_field($filters['post_type'] ?? '');
+               $allowed_post_types = $filters['post_types'] ?? [];
+               if (!is_array($allowed_post_types)) {
+                       $allowed_post_types = explode(',', $allowed_post_types);
+               }
+               $allowed_post_types = array_values(array_filter(array_map('sanitize_key', (array) $allowed_post_types)));
+               if (empty($allowed_post_types)) {
+                       $allowed_post_types = array_values(get_post_types(['public' => true, 'exclude_from_search' => false], 'names'));
+               }
+
+               if ($post_type_param === '' || !in_array($post_type_param, $allowed_post_types, true)) {
+                       $post_type      = $allowed_post_types[0];
+                       $query_post_type = $allowed_post_types;
+               } else {
+                       $post_type      = $post_type_param;
+                       $query_post_type = $post_type;
+               }
+
                $paged          = (int)($filters['paged'] ?? 1);
                $posts_per_page = isset($filters['posts_per_page']) ? (int) $filters['posts_per_page'] : 12;
 
@@ -179,7 +196,7 @@ class Components_FilterAjax {
 
                // 🔍 WP_Query args
                $args = array_merge([
-                       'post_type'      => $post_type,
+                       'post_type'      => $query_post_type,
                        'post_status'    => 'publish',
                        'posts_per_page' => $posts_per_page,
                        'paged'          => $paged,
@@ -217,6 +234,11 @@ class Components_FilterAjax {
                        $fsrc  = $def['source'] ?? 'meta';
                        $key   = $def['name'] ?? $fname;
 
+                       if ($fsrc === 'post_type') {
+                               unset($filter_defs_for_counts[$fname]);
+                               continue;
+                       }
+
                        if ($ftype === 'range') {
                                $def['value'] = [
                                        'min' => $filters['min_' . $key] ?? null,
@@ -247,7 +269,7 @@ class Components_FilterAjax {
                }
                unset($def);
 
-               $global_count_args = ['post_type' => $post_type];
+               $global_count_args = ['post_type' => $query_post_type];
                if (!empty($filters['s'])) {
                        $global_count_args['s'] = sanitize_text_field($filters['s']);
                }
