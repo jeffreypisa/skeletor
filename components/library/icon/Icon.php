2c2a63d2abe06dbebe1dda61d84e97f0648f04ba
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2025-08-19 16:21:33 +0200

    feat: expand Icon component text and styling options

--- components/library/icon/Icon.php
+++ components/library/icon/Icon.php
@@ -25,7 +25,17 @@ class Components_Icon extends Site {
             'library' => 'fontawesome',
             'class' => '',
             'container_height' => 20,
-            'container_width' => 20
+            'container_width' => 20,
+            'title' => '',
+            'url' => '',
+            'title_position' => 'right',
+            'title_level' => 'span',
+            'title_class' => '',
+            'icon_class' => '',
+            'gap' => 10,
+            'target' => 'self',
+            'color_primary' => '',
+            'color_secondary' => ''
         ];
 
         $settings = array_merge($defaults, $args);
@@ -48,7 +58,19 @@ class Components_Icon extends Site {
 
         // Remove existing width and height attributes and force 100% scaling
         $svg = preg_replace('/\s(width|height)="[^"]*"/', '', $svg);
-        $svg = preg_replace('/<svg\b([^>]*)>/', '<svg$1 width="100%" height="100%">', $svg, 1);
+        $icon_class = trim($settings['icon_class']);
+        $color_primary = sanitize_text_field($settings['color_primary']);
+        $color_secondary = sanitize_text_field($settings['color_secondary']);
+        $style_attr = '';
+        if ($color_primary !== '') {
+            $style_attr .= '--fa-primary-color:' . $color_primary . ';';
+        }
+        if ($color_secondary !== '') {
+            $style_attr .= '--fa-secondary-color:' . $color_secondary . ';';
+        }
+        $class_attr = $icon_class !== '' ? ' class="' . esc_attr($icon_class) . '"' : '';
+        $style_attr = $style_attr !== '' ? ' style="' . esc_attr($style_attr) . '"' : '';
+        $svg = preg_replace('/<svg\b([^>]*)>/', '<svg$1' . $class_attr . $style_attr . ' width="100%" height="100%">', $svg, 1);
 
         $container_height = $settings['container_height'];
         if (is_numeric($container_height)) {
@@ -62,11 +84,36 @@ class Components_Icon extends Site {
             $container_width .= 'px';
         }
 
+        $gap = $settings['gap'];
+        if (is_numeric($gap)) {
+            $gap .= 'px';
+        } else {
+            $gap = sanitize_text_field($gap);
+        }
+
+        $allowed_targets = ['self', 'blank'];
+        $target = in_array($settings['target'], $allowed_targets, true) ? '_' . $settings['target'] : '_self';
+
+        $title = sanitize_text_field($settings['title']);
+        $url = esc_url($settings['url']);
+        $allowed_positions = ['left', 'right', 'top', 'bottom'];
+        $title_position = in_array($settings['title_position'], $allowed_positions, true) ? $settings['title_position'] : 'right';
+        $allowed_levels = ['span', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+        $title_level = in_array($settings['title_level'], $allowed_levels, true) ? $settings['title_level'] : 'span';
+        $title_class = sanitize_text_field($settings['title_class']);
+
         $context = [
             'svg' => $svg,
-            'class' => trim('icon ' . $settings['class']),
+            'class' => trim($settings['class']),
             'container_width' => $container_width,
-            'container_height' => $container_height
+            'container_height' => $container_height,
+            'title' => $title,
+            'url' => $url,
+            'title_position' => $title_position,
+            'title_level' => $title_level,
+            'title_class' => $title_class,
+            'gap' => $gap,
+            'target' => $target
         ];
 
         return Timber::compile('icon/icon.twig', $context);
