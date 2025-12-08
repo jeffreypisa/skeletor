344cf4a050d85db76abc36da058e4d11a23407a0
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2025-12-08 13:27:56 +0100

    Support single post accordion items

--- components/library/accordion/Accordion.php
+++ components/library/accordion/Accordion.php
@@ -15,25 +15,58 @@ class Components_Accordion extends Site {
 		return $twig;
 	}
 
-	public function render_accordion($accordion_items, $args = []) {
-		if (!$accordion_items || !is_array($accordion_items)) {
-			return null;
-		}
+        public function render_accordion($accordion_items, $args = []) {
+                if (!$accordion_items) {
+                        return null;
+                }
 
-		$defaults = [
-			'id_prefix' => 'accordion',
-			'icon' => 'chevron',
-			'icon_position' => 'after',
-			'heading_level' => 'h2',
+                if (!is_array($accordion_items)) {
+                        if ($accordion_items instanceof \Timber\Post || $accordion_items instanceof \WP_Post) {
+                                $accordion_items = [$accordion_items];
+                        } else {
+                                return null;
+                        }
+                }
+
+                $accordion_items = array_map(function ($item) {
+                        if (is_array($item) && (isset($item['titel']) || isset($item['tekst']))) {
+                                return $item;
+                        }
+
+                        if ($item instanceof \Timber\Post || $item instanceof \WP_Post) {
+                                return [
+                                        'titel' => $item->post_title ?? '',
+                                        'tekst' => apply_filters('the_content', $item->post_content ?? ''),
+                                ];
+                        }
+
+                        if (is_array($item) && (isset($item['post_title']) || isset($item['post_content']))) {
+                                return [
+                                        'titel' => $item['post_title'] ?? '',
+                                        'tekst' => apply_filters('the_content', $item['post_content'] ?? ''),
+                                ];
+                        }
+
+                        return [
+                                'titel' => '',
+                                'tekst' => '',
+                        ];
+                }, $accordion_items);
+
+                $defaults = [
+                        'id_prefix' => 'accordion',
+                        'icon' => 'chevron',
+                        'icon_position' => 'after',
+                        'heading_level' => 'h2',
 			'heading_class' => '',
 			'icon_weight' => 'solid',
 			'first_item_open' => false,
 		];
 
-		$args = is_array($args) ? array_merge($defaults, $args) : $defaults;
-		$args['accordion_id'] = $args['id_prefix'] . '-' . uniqid();
-		$args['items'] = $accordion_items;
+                $args = is_array($args) ? array_merge($defaults, $args) : $defaults;
+                $args['accordion_id'] = $args['id_prefix'] . '-' . uniqid();
+                $args['items'] = $accordion_items;
 
-		return Timber::compile('accordion/accordion.twig', $args);
-	}
+                return Timber::compile('accordion/accordion.twig', $args);
+        }
 }
